c1fd2c82a745e9a41fec44a63a88f751d139d902
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-09-30 18:44:26 -0500

    feat ( app ) Conecting database with nest using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
@@ -17,6 +21,18 @@ import { ItemsModule } from './items/items.module';
       ]
     }),
     ItemsModule,
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT??5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+
   ],
   controllers: [],
   providers: [],
